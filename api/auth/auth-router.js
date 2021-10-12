eb8d0eaf0e93e4d7ecacd943ce7dae8c304fdd40
Richard Oh <n24512@protonmail.com>
2021-10-12 15:15:10 -0500

    model set up

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,24 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const bcrypt = require("bcryptjs");
+const Users = require("../users/users-model");
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user).then((item) => {
+    res
+      .status(201)
+      .json({
+        username: item.username,
+        password: item.password,
+        role_name: item.role_name,
+      })
+      .catch(next);
+  });
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -15,7 +31,6 @@ router.post("/register", validateRoleName, (req, res, next) => {
     }
    */
 });
-
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
